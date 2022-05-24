13132c9f8ed798775f43881771ef6401385d7455
Evgeniy3 <90893731+Evgeniy3@users.noreply.github.com>
2022-05-25 00:33:01 +0500

    add tests and fix files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,9 +3,12 @@ import { dirname, resolve } from 'path';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
-import buildTree from '../src/buildTree.js';
-import format from '../src/formatters/index.js';
+import buildTree from '../src/tree.js';
+import format from '../src/format/index.js';
 import { getFormat } from '../src/utils.js';
+import parse from '../src/parsers.js';
+import plain from '../src/format/plain.js';
+import stylish from '../src/format/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,11 +23,40 @@ const tests = [
 ];
 
 test.each(tests)('gendiff stylish, plain and json tests', ({
-    file1, file2, output, formatName,
-  }) => {
-    const filepath1 = getFixturePath(file1);
-    const filepath2 = getFixturePath(file2);
-    const expected = readFile(output);
-    const result = genDiff(filepath1, filepath2, formatName);
-    expect(result).toEqual(expected);
-  });
+  file1, file2, output, formatName,
+}) => {
+  const filepath1 = getFixturePath(file1);
+  const filepath2 = getFixturePath(file2);
+  const expected = readFile(output);
+  const result = genDiff(filepath1, filepath2, formatName);
+  expect(result).toEqual(expected);
+});
+
+test('generate tree with wrong format test', () => {
+  const { file1 } = tests[0];
+  const { file2 } = tests[0];
+  const fileContent1 = readFile(file1);
+  const fileContent2 = readFile(file2);
+  const parsedFile1 = parse(fileContent1, getFormat(file1));
+  const parsedFile2 = parse(fileContent2, getFormat(file2));
+  const tree = buildTree(parsedFile1, parsedFile2);
+  const wrongFormat = 'xml';
+  expect(() => format(tree, wrongFormat)).toThrow(`Unknown format to generate a tree: '${wrongFormat}'!`);
+});
+
+test('gendiff with wrong extension test', () => {
+  const file1 = 'file1.xml';
+  const { file2 } = tests[0];
+  const filepath1 = getFixturePath(file1);
+  const filepath2 = getFixturePath(file2);
+  const wrongExtension = getFormat(file1);
+  expect(() => genDiff(filepath1, filepath2)).toThrow(`Unknown format to parse: '${wrongExtension}'!`);
+});
+
+test('build tree with wrong type of node test', () => {
+  const file = 'wrongTypeOfNode.txt';
+  const fileContent = readFile(file);
+  const wrongTypeOfNode = 'undefined';
+  expect(() => plain(fileContent)).toThrow(`Unknown type: '${wrongTypeOfNode}' of node!`);
+  expect(() => stylish(fileContent)).toThrow(`Unknown type: '${wrongTypeOfNode}' of node!`);
+});
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import { readFile } from './utils.js';
+import { readFile, getFormat } from './utils.js';
 import buildTree from './tree.js';
 import format from './format/index.js';
+import parse from './parsers.js';
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const file1 = readFile(filepath1);
 
   const file2 = readFile(filepath2);
 
-  const data1 = JSON.parse(file1);
+  const data1 = parse(file1, getFormat(filepath1));
 
-  const data2 = JSON.parse(file2);
+  const data2 = parse(file2, getFormat(filepath2));
 
   const tree = buildTree(data1, data2);
 
