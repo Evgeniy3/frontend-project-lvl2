2c317477b75d43629f21389773ade7010a8d5bee
Evgeniy3 <90893731+Evgeniy3@users.noreply.github.com>
2022-06-01 22:26:04 +0500

    fixed file

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,9 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
-import { cwd } from 'node:process';
 
 const getFormat = (file) => file.split('.')[1];
 
-const getPath = (file) => resolve(cwd(), file);
+const getPath = (file) => resolve(process.cwd(), file);
 
 const readFile = (file) => readFileSync(getPath(file), 'utf-8');
 
